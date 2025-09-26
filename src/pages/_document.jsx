9ab65736a54acecfeaa1ceb004f2eea9658a68e6
pahae-tests <wahipahae@gmail.com>
2025-09-26 09:41:25 +0100

    Create _document.jsx

---
+++ src/pages/_document.jsx
@@ -0,0 +1,27 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="fr">
+      <Head>
+        {/* Popunder Adsterra */}
+        <script
+          type="text/javascript"
+          src="//pl27725554.revenuecpmgate.com/f1/11/d9/f111d9cdd19bd13a32af38854f23319d.js"
+          async
+        ></script>
+
+        {/* Social Bar Adsterra */}
+        <script
+          type="text/javascript"
+          src="//pl27725653.revenuecpmgate.com/02/f8/b7/02f8b7505ad74d7ee67cd0be3d1de498.js"
+          async
+        ></script>
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
